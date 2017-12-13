30494ad07782e501464dcd05ea4670de174ee8f2
cristiianefaria <cristiane.trc@gmail.com>
2017-12-13 09:20:56 -0200

    Inserindo imagens, e alinhamento do menu

---
+++ header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html class=""><!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>PIZZA NA PEDRA</title>
+
+        <!-- Standard Favicon -->
+        <link rel="icon" type="image/x-icon" href="images//favicon.ico" />
+
+        <!-- For iPhone 4 Retina display: -->
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
+
+        <!-- For iPad: -->
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
+
+        <!-- For iPhone: -->
+        <link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">		
+
+        <!-- Custom - Theme CSS -->
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <!--[if lt IE 9]>
+                <script src="js/html5/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+
+    <body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+        <!-- Loader -->
+        <div id="site-loader" class="load-complete">
+            <div class="loader">
+                <div class="loader-inner ball-clip-rotate">
+                    <div></div>
+                </div>
+            </div>
+        </div><!-- Loader /- -->
+
+
+        <!-- Header Main -->
+        <header class="header-main container-fluid no-padding">	
+            <!-- Container -->
+            <div class="container">
+                <!-- Top Header  -->
+                <div class="top-header container-fluid no-padding">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-6 col-xs-5 our-contacts">
+                            <p class="foco">DELIVERY</p>
+                            <p><i class="fa fa-phone"></i>62 3278-5151</p>
+                            <p><i class="fa fa-whatsapp"></i>62 99235-8068</p>
+                        </div>
+                        <div class="col-md-2 col-sm-2 col-xs-12 logo-block">
+                            <a title="Logo" href="index.html" class="navbar-brand"><img src="images/logo-pizza-na-pedra.png" alt="logo" /></a>
+                        </div>
+                        <div class="col-md-5 col-sm-6 col-xs-7 header-social">
+                            <ul>
+                                <li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                            <ul class="cart">
+                                <li>
+                                    <a aria-expanded="true" aria-haspopup="true" data-toggle="dropdown" id="cart" class="btn dropdown-toggle" title="peça online" href="#">PEÇA ONLINE</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- Top Header /- -->
+
+                <!-- Menu Block -->
+                <div class="menu-block container-fluid no-padding">
+                    <!-- Navigation -->
+                    <nav class="navbar ow-navigation">
+                        <div class="navbar-header">
+                            <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a title="Logo" href="index.html" class="navbar-brand logo1"><img src="images/logo-pizza-na-pedra.png" alt="logo" /></a>
+                        </div>
+                        <div class="navbar-collapse collapse" id="navbar">
+                            <ul class="nav navbar-nav">
+
+                                <li><a href="index.php" title="Services">Home</a></li>
+                                <li><a href="#servicos" title="Serviços">Serviços</a></li>
+                                <li><a title="peça já" href="#pecaja">peça já</a></li>
+                                <li><a title="Sobre nós" href="#sobre">sobre nós</a></li>
+                                <li><a title="Cardápio" href="#cardapio">cardápio</a></li>
+                                <li><a title="Contato" href="#contato">contato</a></li>
+                            </ul>						
+                        </div>
+                    </nav><!-- Navigation -->
+                </div><!-- Menu Block /- -->
+            </div><!-- Container /- -->
+        </header><!-- Header Main /- -->
